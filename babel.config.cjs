b7872d8f4db242188f288c932c7635367ea14091
Sav <maidenfan78@gmail.com>
2025-06-27 07:44:34 +1000

    Minor changes

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,9 @@
 // babel.config.js
 module.exports = {
   presets: [
-    "babel-preset-expo"        // The official Expo Babel preset, with TS support built-in :contentReference[oaicite:0]{index=0}
+    "babel-preset-expo"        
   ],
   plugins: [
-    "@babel/plugin-transform-flow-strip-types"  // Removes Flow annotations from RN dependencies :contentReference[oaicite:1]{index=1}
+    "@babel/plugin-transform-flow-strip-types"  
   ]
 };
