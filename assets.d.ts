ba28d57ad295ce1450a6264bf90bd694d4b3e4f9
Sav <maidenfan78@gmail.com>
2025-06-27 07:57:49 +1000

    Minor changes

--- assets.d.ts
+++ assets.d.ts
@@ -1,4 +1,27 @@
+//declare module "*.png" {
+//  const value: string;
+//  export default value;
+//}
+
+
 declare module "*.png" {
-  const value: string;
+  const value: number;
   export default value;
+}
+
+declare module "*.jpg" {
+  const value: number;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: number;
+  export default value;
+}
+
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
 }
