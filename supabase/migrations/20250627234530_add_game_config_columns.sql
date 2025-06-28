2c6526d46b8ae2b2c3d9b3ab75f6af0d46c89146
Sav <maidenfan78@gmail.com>
2025-06-28 17:10:41 +1000

    Minor changes

--- supabase/migrations/20250627234530_add_game_config_columns.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.games
-  ADD COLUMN main_max integer,
-  ADD COLUMN main_count integer,
-  ADD COLUMN supp_count integer,
-  ADD COLUMN supp_max integer,
-  ADD COLUMN powerball_max integer;
